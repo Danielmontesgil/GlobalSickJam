423081662c802d189eb95e9281c965a4fa45206b
Danielmontesgil <daniel.montes@upb.edu.co>
2018-01-26 23:06:38 -0500

    movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private float speed;
+
+    public PlayerStats playerStats;
+
+    public float horizontal;
+    public float vertical;
+
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    float delta;
+    Vector2 move;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start () {
+        
+
+        speed = playerStats.walkSpeed;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    void FixedUpdate()
+    {
+        delta = Time.deltaTime;
+
+        Move();
+    }
+
+    void Move()
+    {
+        horizontal = Input.GetAxis(StaticsInput.Horizontal);
+        vertical = Input.GetAxis(StaticsInput.Vertical);
+
+        move.x = horizontal;
+        move.y = vertical;
+
+        rb.velocity = move.normalized * speed * delta;
+    }
+
+    void Update () {
+
+        delta = Time.deltaTime;
+
 	}
+
 }
--- Assets/Scripts/ScriptableObjects/PlayerStats.cs
+++ Assets/Scripts/ScriptableObjects/PlayerStats.cs
@@ -3,5 +3,7 @@
 [CreateAssetMenu(menuName ="Characters/Player Stats")]
 public class PlayerStats : ScriptableObject {
 
+    public int lives;
+    public float walkSpeed;
 
 }
