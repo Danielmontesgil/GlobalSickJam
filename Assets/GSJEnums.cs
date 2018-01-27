abe8d57a3d7e149dd01237e8992c372e7ca6d7a7
Unknown <acamilohoyos@hotmail.com>
2018-01-26 23:37:07 -0500

    ScriptableIventoryObjects

---
+++ Assets/GSJEnums.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GSJEnums {
+
+	public enum objectType{
+		soap,
+		syringe, 
+		gas,
+		chinche
+	}
+
+	public enum illnesses
+	{
+		hepatitis,
+		fiebreAmarilla,
+		lupus,
+		tuberculosis,
+		antrax,
+		h1n1,
+		sida
+	}
+}
---
+++ Assets/ObjectsInventory/ObjectsData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "Inventory", menuName = "Objects", order = 1)]
+public class ObjectsData : ScriptableObject {
+	[SerializeField] private List<ObjectInformation> objectsInformation;
+}
+
+[Serializable]
+public class ObjectInformation
+{
+	[SerializeField] public bool helpYou;
+	[SerializeField] public GSJEnums.objectType objectType;
+	[SerializeField] public GSJEnums.illnesses illness;
+	[SerializeField] public Sprite sprite;
+}
