1da5db23cf524e179fd3f348e40c338d16599777
Unknown <acamilohoyos@hotmail.com>
2018-01-27 08:32:08 -0500

    Working on interactions

---
+++ Assets/Scripts/ScriptableObjects/Characters/CharactersData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(menuName ="Characters/Player Data")]
+public class CharactersData : ScriptableObject {
+	[SerializeField] public List<CharactersInformation> characters;
+}
+
+[Serializable]
+public class CharactersInformation
+{
+	[SerializeField] public int index;
+	[SerializeField] public Animator playerAnimator;
+	[SerializeField] public Sprite icon;
+}
