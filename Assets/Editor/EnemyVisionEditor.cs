efaca9ce355653c20055c2104e15a7d5b8d7cd9e
Danielmontesgil <daniel.montes@upb.edu.co>
2018-01-27 02:34:37 -0500

    cone

---
+++ Assets/Editor/EnemyVisionEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(EnemyVision))]
+public class EnemyVisionEditor : Editor
+{
+
+    void OnSceneGUI()
+    {
+        EnemyVision fow = (EnemyVision)target;
+        Handles.color = Color.white;
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.range);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.range);
+    }
+
+}
---
+++ Assets/Scripts/EnemyVision.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVision : MonoBehaviour {
+
+    [Range(0, 360)]
+    public float viewAngle;
+    public float range;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    void Update()
+    {
+        FindVisibleTargets();
+    }
+
+    private void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targertsInViewRadius = Physics.OverlapSphere(transform.position, range, targetMask);
+        RaycastHit hit;
+
+        for (int i = 0; i < targertsInViewRadius.Length; i++)
+        {
+            Transform target = targertsInViewRadius[i].transform;
+            Vector2 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector2.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                if (Physics.Raycast(transform.position, dirToTarget, out hit, range))
+                {
+                    if (hit.transform.name == target.transform.name)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+
+    public virtual Vector2 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
