c2b1372c4a714ef7e1c2dea1d5a8c35b880e6269
LAPTOP-HCMQF8S9\Agustin <pepe_diaz92@hotmail.com>
2018-01-28 11:32:10 -0500

    Creditos

--- Assets/Scripts/tutorial/TutorialManager.cs
+++ Assets/Scripts/tutorial/TutorialManager.cs
@@ -31,17 +31,57 @@ public class TutorialManager : MonoBehaviour {
 		{
 			case 0:
 			    Pantallas[0].SetActive(true);
-				Instrucciones.transform.localPosition = new Vector3(0f,0f,0f);
-				Instrucciones.GetComponent<RectTransform>().sizeDelta = new Vector3(0f,0f,0f);
-				TxtInstrucciones.text="Mueve el personaje con las teclas WASD";
+				Instrucciones.SetActive(true);
+				Instrucciones.transform.localPosition = new Vector3(507.6f,0f,0f);
+				Instrucciones.GetComponent<RectTransform>().sizeDelta = new Vector3(459.3f,230f,0f);
+				TxtInstrucciones.text="Mueve el personaje con las teclas WASD.";
                 yield return new WaitForSeconds(0.1f);
 			break;
 			case 1:
+			    Pantallas[0].SetActive(false);
 			    Pantallas[1].SetActive(true);
-				Instrucciones.transform.localPosition = new Vector3(0f,0f,0f);
-				Instrucciones.GetComponent<RectTransform>().sizeDelta = new Vector3(0f,0f,0f);
-				TxtInstrucciones.text="Mueve el personaje con las teclas WASD";
+				Instrucciones.transform.localPosition = new Vector3(74.8f,37.2f,0f);
+				Instrucciones.GetComponent<RectTransform>().sizeDelta = new Vector3(459.3f,230f,0f);
+				TxtInstrucciones.text="Deveras Buscar items en el mapa para usarlos.";
                 yield return new WaitForSeconds(0.1f);
+			break;
+			case 2:
+			    Pantallas[1].SetActive(false);
+			    Pantallas[2].SetActive(true);
+				Instrucciones.transform.localPosition = new Vector3(32.03f,40f,0f);
+				Instrucciones.GetComponent<RectTransform>().sizeDelta = new Vector3(626.4f,317.1f,0f);
+				TxtInstrucciones.text="Estando al lado de un item presiona <b>F</b> para obtener el item, este aparecera en el recuadro de item abajo a la derecha.";
+                yield return new WaitForSeconds(0.1f);
+			break;
+			case 3:
+			    Pantallas[2].SetActive(false);
+			    Pantallas[3].SetActive(true);
+				Instrucciones.transform.localPosition = new Vector3(-266.96f,40f,0f);
+				Instrucciones.GetComponent<RectTransform>().sizeDelta = new Vector3(650f,319.6f,0f);
+				TxtInstrucciones.text="Teniendo un item debes analizar donde este deve ser usado, al usar un item aparecera un retroalimentación sobre su resultado.";
+                yield return new WaitForSeconds(0.1f);
+			break;
+			case 4:
+			    Pantallas[3].SetActive(false);
+			    Pantallas[4].SetActive(true);
+				Instrucciones.transform.localPosition = new Vector3(-0.5f,185.5f,0f);
+				Instrucciones.GetComponent<RectTransform>().sizeDelta = new Vector3(459.3f,230f,0f);
+				TxtInstrucciones.text="<b>All In</b> aparecerá en el mapa patrullando para preguntar sobre como esta tu juego, evitalo o perderás vida.";
+                yield return new WaitForSeconds(0.1f);
+			break;
+			case 5:
+			    Pantallas[4].SetActive(false);
+			    Pantallas[5].SetActive(true);
+				Instrucciones.transform.localPosition = new Vector3(-0.5f,185.5f,0f);
+				Instrucciones.GetComponent<RectTransform>().sizeDelta = new Vector3(459.3f,230f,0f);
+				TxtInstrucciones.text="Ten cuidado con el tiempo, solo tienes 48 horas.";
+                yield return new WaitForSeconds(0.1f);
+			break;
+			case 6:
+			    BtnSig.interactable=false;
+				Fades[1].SetActive(true);
+                yield return new WaitForSeconds(1.2f);
+				SceneManager.LoadScene("level1");
 			break;
 			
 		}   
